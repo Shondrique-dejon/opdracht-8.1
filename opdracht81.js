3623f7dc7e27eb0bca2fb7f1f1d3dd103f61742c
Shondrique-dejon <9018540@student.zadkine.nl>
2022-11-04 12:25:50 +0100

    opdracht8.1

---
+++ opdracht81.js
@@ -0,0 +1,29 @@
+function Plus(){
+ var getal1=parseInt(document.getElementById("getal1").value);
+ var getal2=parseInt(document.getElementById("getal2").value);
+ var antwoord=getal1+getal2;
+ document.getElementById("antwoord").innerHTML=antwoord;
+}
+function Min(){
+    var getal1=document.getElementById("getal1").value;
+    var getal2=document.getElementById("getal2").value;
+    var antwoord=getal1-getal2;
+    document.getElementById("antwoord").innerHTML=antwoord;
+   }
+   function Delen(){
+    var getal1=document.getElementById("getal1").value;
+    var getal2=document.getElementById("getal2").value;
+    var antwoord=getal1/getal2;
+    document.getElementById("antwoord").innerHTML=antwoord;
+   }
+   function Keer(){
+    var getal1=document.getElementById("getal1").value;
+    var getal2=document.getElementById("getal2").value;
+    var antwoord=getal1*getal2;
+    document.getElementById("antwoord").innerHTML=antwoord;
+   }
+    
+ 
+
+ 
+
