31098e705d9ba7b6897ab12af58a11dca51c3206
Shondrique-dejon <9018540@student.zadkine.nl>
2022-11-07 11:04:30 +0100

    if 8.1

--- opdracht81.js
+++ opdracht81.js
@@ -3,27 +3,40 @@ function Plus(){
  var getal2=parseInt(document.getElementById("getal2").value);
  var antwoord=getal1+getal2;
  document.getElementById("antwoord").innerHTML=antwoord;
+ if (antwoord < 1) {
+    document.getElementById("antwoord").innerHTML = "het getal is te laag";
+  }
 }
 function Min(){
-    var getal1=document.getElementById("getal1").value;
-    var getal2=document.getElementById("getal2").value;
-    var antwoord=getal1-getal2;
+ var getal1=document.getElementById("getal1").value;
+ var getal2=document.getElementById("getal2").value;
+ var antwoord=getal1-getal2;
     document.getElementById("antwoord").innerHTML=antwoord;
-   }
-   function Delen(){
-    var getal1=document.getElementById("getal1").value;
-    var getal2=document.getElementById("getal2").value;
-    var antwoord=getal1/getal2;
+    if (antwoord < 1) {
+        document.getElementById("antwoord").innerHTML = "het getal is te laag ";
+      }
+}
+function Delen(){
+ var getal1=document.getElementById("getal1").value;
+ var getal2=document.getElementById("getal2").value;
+ var antwoord=getal1/getal2;
     document.getElementById("antwoord").innerHTML=antwoord;
-   }
-   function Keer(){
-    var getal1=document.getElementById("getal1").value;
-    var getal2=document.getElementById("getal2").value;
-    var antwoord=getal1*getal2;
+    if (antwoord < 1) {
+        document.getElementById("antwoord").innerHTML = "het getal is te laag";
+      }
+}
+function Keer(){
+ var getal1=document.getElementById("getal1").value;
+ var getal2=document.getElementById("getal2").value;
+ var antwoord=getal1*getal2;
     document.getElementById("antwoord").innerHTML=antwoord;
-   }
-    
- 
+    if (antwoord < 1) {
+        document.getElementById("antwoord").innerHTML = "het getal is telaag";
+      }
+}
+
+
+
 
  
 
